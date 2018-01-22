28e42f76cdd807d1974db218bdf47ac0a4655c89
hodsonus <jhodson@ufl.edu>
2018-01-22 13:13:05 -0500

    initial file creation

---
+++ sparseMatrix.java
@@ -0,0 +1,106 @@
+public class sparseMatrix implements SparseInterface {
+
+  // @Override
+  /*
+      Should clear the matrix of all entries (make all entries 0)
+   */
+  public void clear() {
+
+
+  }
+
+
+  /*
+      Sets maximum size of the matrix, should also clear the matrix (make all elements 0)
+   */
+  public void setSize(int size) {
+
+
+  }
+
+
+  /*
+      Adds an element to the row and column passed as arguments (overwrites if element is already present at that position).
+      Throws an error if row/column combination is out of bounds.
+   */
+  public void addElement(int row, int col, int data) {
+
+
+  }
+
+
+  /*
+      Remove (make 0) the element at the specified row and column.
+      Throws an error if row/column combination is out of bounds.
+   */
+  public void removeElement(int row, int col) {
+
+
+  }
+
+
+  /*
+      Return the element at the specified row and column
+      Throws an error if row/column combination is out of bounds.
+   */
+  public int getElement(int row, int col) {
+
+    return 0;
+  }
+
+  /*
+      Returns the determinant of the matrix calculated recursively (Use the formula provided in the project description).
+   */
+  public int determinant() {
+
+    return 0;
+  }
+
+  /*
+      Returns a new matrix which is the minor of the original (See project description for minor definition).
+   */
+  public SparseInterface minor(int row, int col) {
+
+    return new sparseMatrix();
+  }
+
+  /*
+  Should return the nonzero elements of your sparse matrix as a string.
+  The String should be k lines, where k is the number of nonzero elements.
+  Each line should be in the format "row column data" where row and column are the "coordinate" of the data and all are separated by spaces.
+  An empty matrix should return an empty string.
+  The print should be from left to right and from top to bottom (like reading a book) i.e. the matrix
+
+                                                   3 0 1
+                                                   0 2 0
+                                                   0 0 4
+
+                                               Should print as:
+                                                   0 0 3
+                                                   0 2 1
+                                                   1 1 2
+                                                   2 2 4
+
+   */
+  public String toString() {
+
+    return "";
+  }
+
+
+  /*
+  Should return the size of the matrix.
+   */
+  public int getSize() {
+
+    return 0;
+  }
+}
+
+class tester {
+
+  public static void main(String[] args) {
+
+    System.out.println("hey hows it goin");
+  }
+}
